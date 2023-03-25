b09c6e52ebcf4d8b4b4f158709e4c59a95dad343
Arjun Kahlon <arjunskahlon@gmail.com>
2023-03-25 13:50:10 -0700

    Implemented API route for authenticating user and responding to client
    Email and password is destructured from client request. Req password is
    tested against user password. If a password match, the server responds
    back to the client with user data.

---
+++ src/pages/api/authAPI/userAuth.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { User } from "../../../../types/user";
+import { loadFileContents } from "../../../../utils/load-file";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+
+  if (!req.body) {
+    res.status(404).json({Error: 'Invalid Request'});
+  }
+
+  try {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+      res.status(400).json({Error: 'Email or Password not provided'});
+    }
+
+    const data: string = await loadFileContents('data', '/users.json');
+    const users: User[] = JSON.parse(data);
+
+    const user = users.find(function (user: User) { return user.email === email; });
+
+    if (!user) {
+      res.status(400).json({Error: 'User does not exist'});
+    } else {
+        if (user.password === password) {
+          const { id, firstname, lastname } = user;
+          const resObj = { email, id, firstname, lastname };
+          res.json({ resObj });
+        } else {
+          res.status(401).json({Error: 'Invalid Login Credentials'})
+      }
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ Error: 'An error occured'})
+  }
+}
