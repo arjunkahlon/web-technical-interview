492d0a6806144f27cc22c8c5e1f298366a98099b
Arjun Kahlon <arjunskahlon@gmail.com>
2023-03-25 20:20:54 -0700

    Fix: Corrected SVG stroke attribute naming convention
    Previous naming convention (kebab-case) was triggering warnings in console.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -19,9 +19,9 @@ export default function Card(props : CardProps) {
                                 <p className="inline-block">{home.address}</p>
                             </div>
                             <div className="flex flex-col basis-1/5">
-                                <svg width="30" height="30" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.00024000000000000003" className="inline-block">
-                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.144"></g>
+                                <svg width="30" height="30" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" strokeWidth="0.00024000000000000003" className="inline-block">
+                                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                                    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.144"></g>
                                     <g id="SVGRepo_iconCarrier">
                                         <path d="M16.19 2H7.81C4.17 2 2 4.17 2 7.81V16.18C2 19.83 4.17 22 7.81 22H16.18C19.82 22 21.99 19.83 21.99 16.19V7.81C22 4.17 19.83 2 16.19 2ZM14.79 12.53L11.26 16.06C11.11 16.21 10.92 16.28 10.73 16.28C10.54 16.28 10.35 16.21 10.2 16.06C9.91 15.77 9.91 15.29 10.2 15L13.2 12L10.2 9C9.91 8.71 9.91 8.23 10.2 7.94C10.49 7.65 10.97 7.65 11.26 7.94L14.79 11.47C15.09 11.76 15.09 12.24 14.79 12.53Z" fill="#5FCDB2"></path>
                                     </g>
@@ -33,7 +33,7 @@ export default function Card(props : CardProps) {
                         </div>
                         <div className="flex justify-around text-sm">
                             <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-bed inline-block" width="15" height="15" viewBox="0 0 24 24" stroke-width="1.5" stroke="#696BA4" fill="currentColor" stroke-linecap="round" stroke-linejoin="round">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-bed inline-block" width="15" height="15" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#696BA4" fill="currentColor" strokeLinecap="round" strokeLinejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                     <path d="M3 7v11m0 -4h18m0 4v-8a2 2 0 0 0 -2 -2h-8v6" />
                                     <circle cx="7" cy="10" r="1" />
@@ -41,7 +41,7 @@ export default function Card(props : CardProps) {
                                 <p className="text-xs text-secondary inline-block pl-[0.1rem]">{home.bedrooms}</p>
                             </div>                     
                             <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-calendar inline-block" width="15" height="15" viewBox="0 0 24 24" stroke-width="1.5" stroke="#696BA4" fill="currentColor" stroke-linecap="round" stroke-linejoin="round">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-calendar inline-block" width="15" height="15" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#696BA4" fill="currentColor" strokeLinecap="round" strokeLinejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                     <rect x="4" y="5" width="16" height="16" rx="2" />
                                     <line x1="16" y1="3" x2="16" y2="7" />
@@ -53,7 +53,7 @@ export default function Card(props : CardProps) {
                                 <p className="text-xs text-secondary inline-block pl-[0.1rem]">{home.yearBuilt}</p>
                             </div>
                             <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-bath inline-block" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="#696BA4" fill="currentColor" stroke-linecap="round" stroke-linejoin="round">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-bath inline-block" width="16" height="16" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#696BA4" fill="currentColor" strokeLinecap="round" strokeLinejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                     <path d="M4 12h16a1 1 0 0 1 1 1v3a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4v-3a1 1 0 0 1 1 -1z" />
                                     <path d="M6 12v-7a2 2 0 0 1 2 -2h3v2.25" />
@@ -63,7 +63,7 @@ export default function Card(props : CardProps) {
                                 <p className="text-xs text-secondary inline-block pl-[0.1rem]">{home.bathrooms}</p>
                             </div>
                             <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-home inline-block" width="15" height="15" viewBox="0 0 24 24" stroke-width="1.5" stroke="#696BA4" fill="currentColor" stroke-linecap="round" stroke-linejoin="round">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-home inline-block" width="15" height="15" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#696BA4" fill="currentColor" strokeLinecap="round" strokeLinejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                     <polyline points="5 12 3 12 12 3 21 12 19 12" />
                                     <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
